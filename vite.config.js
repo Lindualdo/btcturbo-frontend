9cf635a0916bed10726125c3a59d58d09cb21007
Lindualdo <lindualdo@hotmail.com>
2025-06-09 15:10:13 +0100

    fix: rotas das paginas

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
+// Arquivo: vite.config.js
+// Localização: btcturbo-frontend/vite.config.js
+
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ciclos: resolve(__dirname, 'ciclos.html'),
+        momentum: resolve(__dirname, 'momentum.html'),
+        tecnico: resolve(__dirname, 'tecnco.html')
+      }
+    }
   },
   server: {
     port: 3000,
