1ce34dac535e14aa2c7ec5e185c6e94397eb38b6
Lindualdo <lindualdo@hotmail.com>
2025-06-19 19:01:35 +0100

    Update api.js

--- src/shared/api.js
+++ src/shared/api.js
@@ -5,7 +5,7 @@ API Client centralizado para todas as páginas
 
 export class ApiClient {
     constructor() {
-        this.baseURL = 'https://btcturbo-v5-production.up.railway.app/api/v2';
+        this.baseURL = 'https://btcturbo-v5-production.up.railway.app/api/v3';
     }
 
     async fetchData(endpoint) {
@@ -27,8 +27,8 @@ export class ApiClient {
     }
 
     // Endpoints específicos
-    async getDashboardHome() { return this.fetchData('dashboard-home'); } // Home
-    async getDashMercado() { return this.fetchData('dash-mercado'); } // Detalhes Mercado
+    async getDashboardHome() { return this.fetchData('dash-main'); } // Home
+    async getDashMercado() { return this.fetchData('analise-mercado'); } // Detalhes Mercado
    // async getMercado() { return this.fetchData('analise-mercado'); }
    // async getRisco() { return this.fetchData('analise-risco'); }
    // async getAlavancagem() { return this.fetchData('analise-alavancagem'); }
